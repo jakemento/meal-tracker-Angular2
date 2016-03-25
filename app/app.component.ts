94ddd4e092494821dd1119943a7de30710bc8ab5
Jake Steinberg <jakesteinberg@gmail.com>
2016-03-25 08:09:51 -0700

    add app component for displaying meals, populate with a couple of starting meals ,write template the meal-list

---
+++ app/app.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter} from 'angular2/core';
+import {MealListComponent} from './meal-list.component';
+import {Meal} from './meal.model';
+
+@Component({
+  selector: 'my-app',
+  directives: [MealListComponent],
+  template: `
+  <div class="container"> <center>
+    <h1 >Meal Tracker </h1>
+    <meal-list
+      [mealList]="meals"
+      (onMealSelect)="mealWasSelected($event)">
+    </meal-list> </center>
+  </div>
+  `
+})
+
+export class AppComponent {
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Fish Tacos", "Tacos with salsa, sour cream and cilantro", 330),
+      new Meal("Steak", "Ribeye with mashed potatoes and green beans", 550),
+      new Meal("Green Curry", "Spicy Green Curry with eggplant and chicken", 480),
+
+
+    ];
+  }
+  mealWasSelected(clickedMeal: Meal): void {
+    console.log(clickedMeal);
+  }
+}
