11cf7c0d6ec893f970c9873cb719426e60d09386
Jake Steinberg <jakesteinberg@gmail.com>
2016-03-25 08:19:36 -0700

    add meal list component and template for it

---
+++ app/meal-list.component.ts
@@ -0,0 +1,42 @@
+import {Component, EventEmitter } from 'angular2/core';
+import {MealComponent } from './meal.component';
+import {Meal } from './meal.model';
+import {EditMealDetailsComponent} from './edit-meal-details.component';
+import {NewMealComponent} from './new-meal.component';
+import {CaloriePipe} from './calorie.pipe';
+
+@Component({
+  selector: 'meal-list',
+  inputs: ['mealList'],
+  outputs: ['onMealSelect'],
+  pipes: [CaloriePipe],
+  directives: [MealComponent, EditMealDetailsComponent, NewMealComponent],
+  template: `
+
+  <div class="keglister">
+    <label class="whiteText"> sort by Calories </label>
+    <div class ="biggerFont">
+      <select (change)="onChange($event.target.value)">
+        <option value="lessthan300">Less than 300 Calories</option>
+        <option value="moreThan300">More Than 300 Calories</option>
+        <option value="all" selected="selected">Show All</option>
+      </select>
+    </div>
+    <meal-display *ngFor="#currentMeal of mealList | calorie:filterDone"
+      (click)="mealClicked(currentMeal)"
+      [class.selected]="currentMeal === selectedMeal"
+      [meal]="currentMeal">
+    </meal-display>
+  </div>
+  <div class="border">
+    <div class="col-lg-6">
+      <center> <new-meal (onSubmitNewMeal)="createMeal($event)"></new-meal> </center>
+    </div>
+
+    <div class="col-lg-6">
+      <edit-meal-details *ngIf="selectedMeal" [meal]="selectedMeal">
+      </edit-meal-details>
+    </div>
+  </div>
+  `
+})
