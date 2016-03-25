b9767f7ca059a08b7210d8b10cf835ae6e99d5b5
Jake Steinberg <jakesteinberg@gmail.com>
2016-03-25 08:50:18 -0700

    add calorie pipe

---
+++ app/calorie.pipe.ts
@@ -0,0 +1,24 @@
+import {Pipe, PipeTransform} from 'angular2/core';
+import {Meal } from './meal.model';
+
+@Pipe({
+  name: "calorie",
+  pure: false
+})
+
+export class CaloriePipe implements PipeTransform {
+  transform(input: Meal[], args) {
+    var desiredDoneState =args[0];
+    if(desiredDoneState === "moreThan300") {
+      return input.filter((meal) => {
+        return meal.calories > 300;
+      });
+    } else if (desiredDoneState ===  "lessThan300") {
+      return input.filter((meal) => {
+        return meal.calories < 300;
+      });
+    } else {
+      return input;
+    }
+  }
+}
